cf2d17148fe390350fa192a485540a080d4ff697
Tuan Ngo <tuannaru1993@gmail.com>
2021-09-11 23:56:58 +0700

    Add prettier plugin & Implemented search github account

--- scripts/app.js
+++ scripts/app.js
@@ -1,24 +1,89 @@
-const axios = require('axios');
+const axios = require("axios");
 
-let searchInput = document.getElementById('user-search-app');
+function handleResults(error) {
+  let noResults = document.querySelector(".no-results");
+  if (!error) {
+    noResults.classList.add("hidden");
+  } else {
+    noResults.classList.remove("hidden");
+  }
+}
 
-axios.get('https://api.github.com/users/octocat')
-    .then(response => {
-        const userData = response.data;
-        const createDate = new Date(Date.parse(userData.created_at)).toLocaleDateString("en-US", { year: 'numeric', month: 'short', day: 'numeric' })
-        console.log(userData);
-        let content = searchInput.innerHTML;
-        content = content.replace(/{%USER_NAME%}/g, userData.name);
-        content = content.replace(/{%LOGIN_NAME%}/g, userData.login);
-        content = content.replace(/{%AVATAR%}/g, userData.avatar_url);
-        content = content.replace(/{%CREATE_DATE%}/g, createDate);
-        content = content.replace(/{%BIO%}/g, userData.bio || '');
-        content = content.replace(/{%REPOS%}/g, userData.public_repos);
-        content = content.replace(/{%FOLLOWERS%}/g, userData.followers);
-        content = content.replace(/{%FOLLOWING%}/g, userData.following);
-        content = content.replace(/{%LOCATION%}/g, userData.location);
-        content = content.replace(/{%SOCIAL_MEDIA%}/g, userData.twitter_username || 'Not Available');
-        content = content.replace(/{%GITHUB_LINK%}/g, userData.blog);
-        content = content.replace(/{%COMPANY%}/g, userData.company);
-        searchInput.innerHTML = content;
-    })
+function handleCheckAvailable(selector, data) {
+  if (data) {
+    selector.textContent = data;
+    selector.parentNode.classList.remove("opacity-30");
+    if (selector.tagName === "A") {
+      selector.href = data;
+      selector.title = data;
+      selector.classList.remove("pointer-events-none");
+    }
+  } else {
+    selector.textContent = "Not Available";
+    selector.parentNode.classList.add("opacity-30");
+    if (selector.tagName === "A") {
+      selector.classList.add("pointer-events-none");
+    }
+  }
+}
+
+function handleRenderUser(userData) {
+  if (!userData) return;
+
+  let avatar = document.querySelector(".avatar");
+  let userName = document.querySelector(".user-name");
+  let loginName = document.querySelector(".login-name");
+  let joinDate = document.querySelector(".join-date");
+  let bio = document.querySelector(".bio");
+  let repos = document.querySelector(".repos");
+  let followers = document.querySelector(".followers");
+  let following = document.querySelector(".following");
+  let location = document.querySelector(".location");
+  let twitter = document.querySelector(".twitter");
+  let githubLink = document.querySelector(".github-link");
+  let company = document.querySelector(".company");
+  let items = document.querySelectorAll(".item");
+  const createDate = new Date(
+    Date.parse(userData.created_at)
+  ).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "short",
+    day: "numeric",
+  });
+
+  avatar.src = userData.avatar_url;
+  userName.textContent = userData.name;
+  loginName.textContent = `@${userData.login}`;
+  joinDate.textContent = createDate;
+  bio.textContent = userData.bio;
+  repos.textContent = userData.public_repos;
+  followers.textContent = userData.followers;
+  following.textContent = userData.following;
+  handleCheckAvailable(location, userData.location);
+  handleCheckAvailable(twitter, userData.twitter_username);
+  handleCheckAvailable(githubLink, userData.blog);
+  handleCheckAvailable(company, userData.company);
+}
+
+function handleChange(e) {
+  if (e.target.value === "") return;
+
+  handleGetUser(e.target.value).then((user) => handleRenderUser(user));
+}
+
+async function handleGetUser(user = "octocat") {
+  try {
+    const response = await axios.get(`https://api.github.com/users/${user}`);
+    handleResults();
+    return response.data;
+  } catch (error) {
+    handleResults(error);
+  }
+}
+
+handleGetUser().then((user) => handleRenderUser(user));
+
+let searchInput = document.querySelector(".search-input");
+let searchBtn = document.querySelector(".search-btn");
+searchInput.addEventListener("change", handleChange);
+searchBtn.addEventListener("click", searchInput.onchange);
