909bb48437465303b61c66a818c6cca858766a45
Tuan Ngo <tuannaru1993@gmail.com>
2021-09-12 19:36:22 +0700

    add missing plugins

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {
+      filter: ['dark'],
+      brightness: ['dark'],
+      invert: ['dark']
+    },
+  },
+  plugins: [],
+};
